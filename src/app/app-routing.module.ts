cf87f91a0d0a1fbdab8e4bbe6ccb5715d307a566
SoftPymesCgarzon <desarrollo.cristian.garzon@gmail.com>
2023-03-20 22:55:34 -0500

    import authModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthModule } from './auth/auth.module';
+import { HotelFormComponent } from './components/forms/hotel-form/hotel-form.component';
+import { ReservationFormComponent } from './components/forms/reservation-form/reservation-form.component';
+import { ReservationListComponent } from './components/reservation-list/reservation-list.component';
 import { AuthGuard } from './guards/auth.guard';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { HotelDetailPageComponent } from './pages/hotel-detail-page/hotel-detail-page.component';
-import { HotelsPageComponent } from './pages/hotels-page/hotels-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 
@@ -22,16 +25,21 @@ const routes: Routes = [
     component: HomePageComponent,
   },
   {
-    path:'hotels',
-    component: HotelsPageComponent,
+    path:'reservation/:id',
+    component: ReservationFormComponent,
   },
   {
     path:'hotels/:id',
     component: HotelDetailPageComponent,
   },
   {
-    path:'logueado',
-    component: HotelsPageComponent,
+    path:'createHotel',
+    component: HotelFormComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path:'reservationsList',
+    component: ReservationListComponent,
     canActivate: [AuthGuard]
   },
   {
@@ -41,7 +49,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), AuthModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
