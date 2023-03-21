5cb73d73d671c1dd5753736c19dbf0529ca53440
SoftPymesCgarzon <desarrollo.cristian.garzon@gmail.com>
2023-03-20 22:56:00 -0500

    creating authModule

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+import { LoginFormComponent } from '../components/forms/login-form/login-form.component';
+import { LoginPageComponent } from '../pages/login-page/login-page.component';
+import { MaterialModule } from '../modules/material/material.module';
+
+
+
+@NgModule({
+  declarations: [
+    LoginFormComponent,
+    LoginPageComponent
+  ],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    ReactiveFormsModule
+  ]
+})
+export class AuthModule { }
