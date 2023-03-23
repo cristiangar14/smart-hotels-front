f390dab53eb1807304db86af36fbbf7b20e92410
SoftPymesCgarzon <desarrollo.cristian.garzon@gmail.com>
2023-03-22 23:35:01 -0500

    rename files

--- src/app/models/hotel.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface IHotel {
-  _id: string;
-  name: string;
-  numberRooms: number;
-  description: string;
-
-}
--- src/app/models/roomType.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface IRoomType {
-  name: string;
-  description: string;
-  capacity: number;
-  price: number;
-  tax: number;
-  code: string;
-
-}
