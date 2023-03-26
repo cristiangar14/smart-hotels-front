cdc74da4ca9d4f20c72fb5c1d274d5e032540b05
SoftPymesCgarzon <desarrollo.cristian.garzon@gmail.com>
2023-03-26 18:39:37 -0500

    starting bookingGuard

---
+++ src/app/guards/booking.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookingGuard implements CanActivate {
+  constructor(private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      const hasHotel = sessionStorage.getItem('hotel');
+
+      if (hasHotel) {
+        return true;
+      }
+
+      this.router.navigate(['/home']);
+      return false;
+  }
+
+}
