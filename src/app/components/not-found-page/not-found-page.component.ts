20d1693884740c7e2d503a6addb665a96f990959
SoftPymesCgarzon <desarrollo.cristian.garzon@gmail.com>
2023-03-27 00:18:01 -0500

    not found

--- src/app/components/not-found-page/not-found-page.component.ts
+++ src/app/components/not-found-page/not-found-page.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-not-found-page',
   templateUrl: './not-found-page.component.html',
   styleUrls: ['./not-found-page.component.scss']
 })
-export class NotFoundPageComponent {}
+export class NotFoundPageComponent {
+  constructor(
+      private router: Router
+    ){}
+
+  goHome(){
+    this.router.navigate([`/home`])
+  }
+
+}
